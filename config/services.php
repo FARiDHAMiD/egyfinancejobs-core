8568ce61a11c430f1d3ba82d8e4ded0e38ffdfea
FARiDHAMiD <faridmohamedfarid@gmail.com>
2025-01-04 21:58:11 +0200

    git remote remove origin

--- config/services.php
+++ config/services.php
@@ -30,6 +30,17 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google' => [
+
+        'client_id' => '414636721224-9ca4udj6589k2ekbat1c075b6jsevrk3.apps.googleusercontent.com',
+
+        'client_secret' => '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',
+
+        // 'redirect' => 'https://egyfinancejobs.com/auth/google/callback',
+        'redirect' => 'http://localhost:8000/auth/google/callback',
+        // 'redirect' => 'http://farid.local:8000/auth/google/callback',
+
+    ],
 
     'recaptcha' => [
         'key' => env('RECAPTCHA_SITE_KEY'),
